9d5955b63a04dc17bbb96b73e997b086eb330a35
Ahmed Issa <ahissa@ucsd.edu>
2025-05-03 11:04:32 -0700

    expose chnages

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -15,12 +15,15 @@ function init() {
     const value = hornSelect.value;
     hornImage.src = `assets/images/${value}.svg`;
     hornSound.src = `assets/audio/${value}.mp3`;
+
+    console.log("Selected horn:", value);
+    console.log("Audio src set to:", hornSound.src);
   });
 
   volumeSlider.addEventListener('input', () => {
     const volume = volumeSlider.value;
     hornSound.volume = volume / 100;
-    
+
     if (volume == 0) {
       volumeImage.src = 'assets/icons/volume-level-0.svg';
     } else if (volume < 33) {
@@ -30,12 +33,21 @@ function init() {
     } else {
       volumeImage.src = 'assets/icons/volume-level-3.svg';
     }
+
+    console.log("Volume set to:", volume);
+    console.log("Audio volume:", hornSound.volume);
   });
 
   playButton.addEventListener('click', () => {
-    hornSound.play();
-    if (hornSelect.value === 'party-horn') {
-      jsConfetti.addConfetti();
+    if (hornSound.src && hornSound.src !== '') {
+      hornSound.play();
+      console.log("Playing sound:", hornSound.src);
+
+      if (hornSelect.value === 'party-horn') {
+        jsConfetti.addConfetti();
+      }
+    } else {
+      console.log("No horn selected.");
     }
   });
 }
